a44246c8cea49d57d4884ef176e14917d51df946
JunSeoYoun <74352371+Fiso-Maker@users.noreply.github.com>
2022-05-06 10:57:49 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 460 ms, Memory: 8812 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/README.md
@@ -0,0 +1,12 @@
+# [Silver V] 수 정렬하기 2 - 2751 
+
+[문제 링크](https://www.acmicpc.net/problem/2751) 
+
+### 성능 요약
+
+메모리: 8812 KB, 시간: 460 ms
+
+### 분류
+
+정렬(sorting)
+
---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int com(const void *a, const void *b)
+{
+	int na = *(const int*) a;
+	int nb = *(const int*) b;
+
+	if (na < nb)
+		return -1;
+	if (na > nb)
+		return 1;
+	return 0;
+}
+int main()
+{
+	int count;
+	int i;
+	int ch[1000000] = { 0, };
+
+	scanf("%d", &count);
+
+	for (i = 0; i < count; i++)
+	{
+		scanf("%d", &ch[i]);
+	}
+	
+	qsort(ch, count, sizeof(ch[0]), com);
+
+	for (i = 0; i < count; i++)
+	{
+		printf("%d\n", ch[i]);
+	}
+}
