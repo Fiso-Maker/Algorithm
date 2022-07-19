69cad3b7677e5f89fcd522b26e649298e523b1a4
JunSeoYoun <74352371+Fiso-Maker@users.noreply.github.com>
2022-07-19 14:17:59 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    long long N,M;
+    cin >> N >> M;
+
+    cout << llabs(N-M) << endl;
+}
